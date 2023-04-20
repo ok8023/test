4056a5e8e4c90ff038f7270004c33967761ea36c
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-20 17:17:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "Replit一键测试HTML&PHP脚本"
 echo "脚本作者：ok8023"
 echo "开源地址：https://github.com/ok8023/test/"
-git clone https://github.com/ok8023/test && mv -b test/code/* ./ && mv -b test/code/.[^.]* ./ && rm -rf *~ && rm -rf test/code && rm -rf index.php && rm -rf install.sh && rm -rf README.md
+git clone https://github.com/ok8023/test && mv -b test/code/* ./ && mv -b test/code/.[^.]* ./ && rm -rf *~ && rm -rf test/code && rm -rf /index.php && rm -rf /install.sh && rm -rf /README.md
 echo "资源部署完毕"
 echo "点击Run运行！"
